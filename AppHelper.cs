9769404270c3094b3ace1c19c348bac3b6ce8755
Mehemmedeli Babayev <mahammadalishb@code.edu.az>
2025-08-15 21:46:57 +0400

    Update AppHelper.cs

--- AppHelper.cs
+++ AppHelper.cs
@@ -4,9 +4,37 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Management.Models
+namespace Management
 {
-    internal class AppHelper
+    internal static class AppHelper
     {
+        private static int _nums;
+        public static int Nums
+        {
+            get { return _nums; }
+
+            set { _nums = value; }
+
+        }
+
+
+        public static bool CalculateAverage(int a, int b, int c, int NumAverage)
+        {
+            for (int i = 0; i < _nums; i++)
+            {
+                if (NumAverage == 0)
+                {
+                    Console.WriteLine("Deyer verilmeyib");
+                    return false;
+                }
+                else
+                {
+                    NumAverage = (a + b + c) / 3;
+                    return true;
+                }
+            }
+            return false;
+
+        }
     }
 }
