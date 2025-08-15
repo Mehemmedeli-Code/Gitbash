5b19fdcd1189aa0bb7020a490d140e5e84b7beb1
Mehemmedeli Babayev <mahammadalishb@code.edu.az>
2025-08-15 21:49:15 +0400

    Update ICoop.cs

--- Interfaces/ICoop.cs
+++ Interfaces/ICoop.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +6,18 @@ using System.Threading.Tasks;
 
 namespace Management.Interfaces
 {
-    internal interface Interface1
+    internal interface ICoop
     {
+        public bool IsKeyboardAllowed { get; set; }
+
+        public static bool StartCoop(bool startCoop)
+        {
+            if (startCoop) 
+            {
+                Console.WriteLine("co-op oyun basladildi");
+                return true;
+            }
+            return false;
+        }
     }
 }
