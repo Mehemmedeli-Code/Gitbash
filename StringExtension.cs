d2056eceb80879d3feaa58744995f3b36617c826
Mehemmedeli Babayev <mahammadalishb@code.edu.az>
2025-08-15 21:48:12 +0400

    Update StringExtension.cs

--- StringExtension.cs
+++ StringExtension.cs
@@ -1,12 +1,36 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Management.Models
+namespace Management
 {
     internal class StringExtension
     {
+        public static string Word { get; set; }
+
+
+
+
+        public static string Capitalize(string word)
+        {
+            word = word.Substring(0,1).ToUpper() + word.Substring(1).ToLower();
+            return word;
+        }
+
+        public static void ToUpper(string word)
+        {
+            word = word.Trim();
+            if(word.Length < 1)
+            {
+                Console.WriteLine(" ");
+            }
+            else
+            {
+                word = word.Substring(0, 1).ToUpper() + word.Substring(1).ToLower();
+            }
+        }
+
     }
 }
