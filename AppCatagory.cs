17182cd867aa6c129ec6e1225c36f91943adcdf3
Mehemmedeli Babayev <mahammadalishb@code.edu.az>
2025-08-15 21:46:21 +0400

    Update AppCatagory.cs

--- AppCatagory.cs
+++ AppCatagory.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +6,10 @@ using System.Threading.Tasks;
 
 namespace Management
 {
-    internal class Enum
+    enum AppCatagory
     {
+        game = 0,
+        productivity,
+        social
     }
 }
